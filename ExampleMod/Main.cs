5c90995772159f7a944c612e60032b58e7bc805d
sbcomputertech <sbcomputertech.0@gmail.com>
2022-11-26 21:55:33 +0000

    init

---
+++ ExampleMod/Main.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ExampleMod
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string ModName = "7in7 Day 1";
+        private const string ModAuthor  = "reddust9";
+        private const string ModGuid = "com.reddust9.7in7.day1";
+        private const string ModVersion = "1.0.0";
+        internal void Awake()
+        {
+            // Creating new harmony instance
+            var harmony = new Harmony(ModGuid);
+
+            // Applying patches
+            harmony.PatchAll();
+            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
+        }
+
+        private void Update()
+        {
+            SurvivalMode.instance.Lives = 99;
+        }
+    }
+}
